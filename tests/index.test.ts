82b1e2855cfd7bb5bfe7761e36e3ac0ff6a1484a
eliasinacio <0.elias.inacio@gmail.com>
2022-08-16 08:55:27 -0300

    organize

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,7 @@
 import { Product } from "../src/classes/Product"
 import * as main from "../src/index"
 import { productsDataBase } from "../src/data/products"
+import { Cart } from "../src/classes/Cart"
 
 describe('Product Class', () => {
     test('create a new Product', () => {
@@ -48,5 +49,51 @@ describe('index.ts', () => {
         const expectedValue = 'Clean Code Book'
 
         expect(name).toEqual(expectedValue);
+    })
+})
+
+describe('Cart', () => {
+    test('Cart starts empty', () => {
+        const mainCart = new Cart()
+        const cartProducts = mainCart.getProducts()
+        
+        expect(cartProducts.length).toEqual(0);
+    })
+
+    test('Adding a product to cart, cart is not empty', () => {
+        const mainCart = new Cart()
+        const book = new Product('Book - The Hobbit', 12345, 78.89)
+        
+        mainCart.add(book)
+        
+        const cartProducts = mainCart.getProducts()
+        
+        expect(cartProducts.length).not.toEqual(0);
+    })
+
+    test('Adding and removing product, cart is empty', () => {
+        const mainCart = new Cart()
+        const book = new Product('Book - The Hobbit', 12345, 78.89)
+        
+        mainCart.add(book)
+
+        mainCart.remove(book.id)
+        
+        const cartProducts = mainCart.getProducts()
+        
+        expect(cartProducts.length).toEqual(0);
+    })
+
+    test('try removing id not in cart', () => {
+        const mainCart = new Cart()
+        const book = new Product('Book - The Hobbit', 12345, 78.89)
+        
+        mainCart.add(book)
+
+        mainCart.remove(999)
+        
+        const cartProducts = mainCart.getProducts()
+        
+        expect(cartProducts.length).not.toEqual(0);
     })
 })
